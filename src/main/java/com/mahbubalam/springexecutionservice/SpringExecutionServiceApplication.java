929d931dda10fa9327cb0bdb82dfa349086aef8f
iammahbubalam <mdmahbubnirjon@gmail.com>
2024-03-24 05:33:58 +0600

    Initial commit

---
+++ src/main/java/com/mahbubalam/springexecutionservice/SpringExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mahbubalam.springexecutionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExecutionServiceApplication.class, args);
+	}
+
+}
